6c3d00ca95824f448120e79de90f84781841b2ef
Darlene <112112350+darlenepg@users.noreply.github.com>
2022-11-03 12:52:59 -0230

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,18 +1,21 @@
-
-
 <?php
+$host = 'projectserver.mysql.database.azure.com';
+$username = 'darn84';
+$password = '04Malachi19';
+$db_name = 'sfhdb';
 
-/* database connection */
+//Initializes MySQLi
+$con = mysqli_init();
 
-session_start();
- 
-$host = "projectserver.mysql.database.azure.com"; /* Host name */
-$username = "darn84"; /* User */
-$password = "04Malachi19"; /* Password */
-$dbname = "sfhdb"; /* Database name */
+mysqli_ssl_set($con,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
 
-$con = mysqli_connect($host, $username, $password,$dbname);
-// Check connection
-if (!$con) {
-  die("Connection failed: " . mysqli_connect_error());
+// Establish the connection
+mysqli_real_connect($con, 'projectserver.mysql.database.azure.com', 'darn84', '04Malachi19', 'sfhdb', 3306, NULL, MYSQLI_CLIENT_SSL);
+
+//If connection failed, show the error
+if (mysqli_connect_errno())
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
+
+?>
