6b3e4f3d5c5ec328b2c2983c82c9ecfff19c2861
Darlene <112112350+darlenepg@users.noreply.github.com>
2022-11-03 12:22:48 -0230

    Update connection.php

--- connection.php
+++ connection.php
@@ -5,6 +5,6 @@
 
 $con = mysqli_init();
 mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
-mysqli_real_connect($conn, "projectserver.mysql.database.azure.com", "darn84", "04Malachi19", "sfhdb", 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($con, "projectserver.mysql.database.azure.com", "darn84", "04Malachi19", "sfhdb", 3306, MYSQLI_CLIENT_SSL);
  
 
